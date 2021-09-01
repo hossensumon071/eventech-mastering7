99da64f7114714b7fdcf40404ea67aa483b5966b
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2021-09-01 22:34:35 +0600

    eventech master laravel

---
+++ resources/views/pages/app.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from themewinter.com/demo/html/eventor/why-attend.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 11 Feb 2019 08:24:10 GMT -->
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+	<!-- Site Title -->
+	<title>Eventor -  Conference &amp; Event HTML Template</title>
+
+	<!--Favicon-->
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	
+	<!-- CSS
+	================================================== -->
+	
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Template styles-->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive styles-->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Animation -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Colorbox -->
+	<link rel="stylesheet" href="css/colorbox.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+	
+<body>
+
+	<div class="body-inner">
+
+	<!-- Header start -->
+    @include('include.header')
+	<!--/ Header end -->
+
+	
+
+	<!-- Footer end -->
+
+
+	<!-- Javascript Files
+	================================================== -->
+
+	<!-- initialize jQuery Library -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<!-- Bootstrap jQuery -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- Counter -->
+	<script type="text/javascript" src="js/jquery.counterup.min.js"></script>
+	<!-- Waypoints -->
+	<script type="text/javascript" src="js/waypoints.min.js"></script>
+	<!-- Color box -->
+	<script type="text/javascript" src="js/jquery.colorbox.js"></script>
+	<!-- Smoothscroll -->
+	<script type="text/javascript" src="js/smoothscroll.js"></script>
+	<!-- Animated Pie chart-->
+	<script type="text/javascript" src="js/jquery.easypiechart.js"></script>
+	<!-- Template custom -->
+	<script type="text/javascript" src="js/custom.js"></script>
+
+	</div><!-- Body inner end -->
+</body>
+
+<!-- Mirrored from themewinter.com/demo/html/eventor/why-attend.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 11 Feb 2019 08:24:16 GMT -->
+</html>
